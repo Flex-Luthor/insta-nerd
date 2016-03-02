598620fc13c802ed4705617835d7ffe31ba037f7
Nikola Novakovic <nikola.novakovic9@gmail.com>
2016-03-02 07:41:45 -0500

    Add basic tab layout

---
+++ Feed.js
@@ -0,0 +1,23 @@
+'use strict';
+ 
+import React, {
+  AppRegistry,
+  Component,
+  View,
+  Text 
+} from 'react-native';
+
+class Feed extends Component {
+ 
+  render() {
+    return (
+      <View>
+        <Text>
+          This is an awesome feed
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default Feed;
---
+++ Post.js
@@ -0,0 +1,23 @@
+'use strict';
+ 
+import React, {
+  AppRegistry,
+  Component,
+  View,
+  Text 
+} from 'react-native';
+
+class Post extends Component {
+ 
+  render() {
+    return (
+      <View>
+        <Text>
+          Wanna nerd out m8? 
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default Post;
