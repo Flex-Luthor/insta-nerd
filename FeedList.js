619866122235f902251d03d1791e66afd16924eb
Nikola Novakovic <nikola.novakovic9@gmail.com>
2016-03-03 20:34:00 -0500

    Implement AsyncStorage

--- FeedList.js
+++ FeedList.js
@@ -3,6 +3,7 @@
 import React, {
   StyleSheet,
   AppRegistry,
+  AsyncStorage,
   Component,
   View,
   Text 
@@ -16,11 +17,27 @@ const styles = StyleSheet.create({
 });
 
 class FeedList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      titleOfPic: '',
+      descOfPic: '',
+      picUrl: '' 
+    } 
+  }
+
+  componentDidMount() {
+    AsyncStorage.getItem("picUrl").then((value) => {
+      this.setState({picUrl: value})
+    }) 
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>
           This is an awesome feed
+          {this.state.picUrl}
         </Text>
       </View>
     );
--- PostForm.js
+++ PostForm.js
@@ -1,27 +1,110 @@
 'use strict';
  
 import React, {
-  StyleSheet,
-  AppRegistry,
-  Component,
   View,
-  Text 
+  StyleSheet,
+  Text,
+  Component,
+  TextInput,
+  TouchableHighlight,
+  ActivityIndicatorIOS,
+  AsyncStorage
 } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     marginTop: 50,
     padding: 10
+  },
+  searchInput: {
+    height: 36,
+    marginTop: 10,
+    marginBottom: 10,
+    fontSize: 18,
+    borderWidth: 1,
+    flex: 1,
+    borderRadius: 4,
+    padding: 5
+  },
+  button: {
+    height: 36,
+    backgroundColor: '#f39c12',
+    borderRadius: 8,
+    justifyContent: 'center',
+    marginTop: 15
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  instructions: {
+    fontSize: 18,
+    alignSelf: 'center',
+    marginBottom: 15
+  },
+  fieldLabel: {
+    fontSize: 15,
+    marginTop: 15
+  },
+  errorMessage: {
+    fontSize: 15,
+    alignSelf: 'center',
+    marginTop: 15,
+    color: 'red'
   }
 });
 
 class PostForm extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      titleOfPic: '',
+      descOfPic: '',
+      picUrl: '' 
+    } 
+  }
+
+  pictureTitleInput(event) {
+    this.setState({titleOfPic: event.nativeEvent.text})
+  }
+
+  descriptionOfPictureInput(event) {
+    this.setState({descOfPic: event.nativeEvent.text})
+  }
+
+  urlOfPictureInput(event) {
+    this.setState({picUrl: event.nativeEvent.text})
+  }
+
+  postPicture() {
+    AsyncStorage.setItem("postTitle", this.state.titleOfPic)
+    AsyncStorage.setItem("descOfPic", this.state.descOfPic)
+    AsyncStorage.setItem("picUrl", this.state.picUrl)
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>
-          Wanna nerd out m8? 
-        </Text>
+        <View>
+          <Text style={styles.fieldLabel}>Picture Title:</Text>
+          <TextInput style={styles.searchInput} onChange={this.pictureTitleInput.bind(this)} />
+        </View>
+        <View>
+          <Text style={styles.fieldLabel}>Description of the picture:</Text>
+          <TextInput style={styles.searchInput} onChange={this.descriptionOfPictureInput.bind(this)} />
+        </View>
+        <View>
+          <Text style={styles.fieldLabel}>Url of the picture:</Text>
+          <TextInput style={styles.searchInput} onChange={this.urlOfPictureInput.bind(this)} />
+        </View>
+        <TouchableHighlight style={styles.button}
+          underlayColor='#f1c40f'
+          onPress={this.postPicture.bind(this)}>
+          <Text style={styles.buttonText}>Post</Text>
+        </TouchableHighlight>
+        <Text style={styles.errorMessage}> {this.state.errorMessage} </Text>
       </View>
     );
   }
